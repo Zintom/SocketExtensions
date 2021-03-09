aa1fb390a1e44ef8b4e5b59c093a253a238592f6
Zintom <tzsinkoapps@gmail.com>
2021-03-09 20:47:59 +0000

    Created Demo Project

---
+++ DemoProject/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Net.Sockets;
+using Zintom.SocketExtensions;
+
+namespace DemoProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
