2c4c209e31c380c239252977f5a7f41f2b90da44
Zintom <tzsinkoapps@gmail.com>
2021-03-09 20:48:32 +0000

    Added LengthPrefixedSocket and the TaskSocketExtensions.

---
+++ SocketExtensions/LengthPrefixedSocket.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace Zintom.SocketExtensions
+{
+    /// <summary>
+    /// A wrapper around <see cref="System.Net.Sockets.Socket"/> which provides a way to send data back and forth
+    /// in a length-prefixed and async manner.
+    /// </summary>
+    public class LengthPrefixedSocket
+    {
+
+        /// <summary>
+        /// Holds the state of a receive operation.
+        /// </summary>
+        private class ReceiveState
+        {
+            /// <summary>
+            /// The TCS which represents the completion of this receive operation.
+            /// </summary>
+            internal TaskCompletionSource<byte[]> TaskCompletionSource;
+
+            internal SocketFlags SocketFlags;
+
+            /// <summary>
+            /// The buffer used to receive data into (could be the length prefix or the final data).
+            /// </summary>
+            internal byte[] DataBuffer;
+
+            /// <summary>
+            /// The number of bytes read into DataBuffer so far.
+            /// </summary>
+            internal int BytesRead;
+
+            public ReceiveState(TaskCompletionSource<byte[]> taskCompletionSource, byte[] dataBuffer)
+            {
+                TaskCompletionSource = taskCompletionSource;
+                DataBuffer = dataBuffer;
+            }
+        }
+
+        private readonly Socket _socket;
+
+        public Socket Socket { get => _socket; }
+
+        public LengthPrefixedSocket(Socket underlyingSocket)
+        {
+            _socket = underlyingSocket;
+        }
+
+        #region Operator Overloads
+        public static implicit operator Socket(LengthPrefixedSocket lps)
+        {
+            return lps.Socket;
+        }
+
+        public static explicit operator LengthPrefixedSocket(Socket underlyingSocket)
+        {
+            return new LengthPrefixedSocket(underlyingSocket);
+        }
+        #endregion
+
+        /// <summary>
+        /// Sends data to the socket in a length prefixed manner.
+        /// </summary>
+        /// <param name="buffer">The byte buffer which contains the data to send.</param>
+        /// <remarks><b>Warning:</b> Never use the base <see cref="System.Net.Sockets.Socket"/> Send/Receive methods
+        /// in conjunction with this class as they can corrupt the state.</remarks>
+        /// <returns>The number of bytes sent to the socket.</returns>
+        public Task<int> SendAsync(byte[] buffer, SocketFlags socketFlags)
+        {
+            // Get the length of the data we want to send, then combine that as a prefix to the actual data.
+            byte[] lengthPrefix = BitConverter.GetBytes(buffer.Length);
+
+            byte[] combined = new byte[lengthPrefix.Length + buffer.Length];
+            Array.Copy(lengthPrefix, 0, combined, 0, lengthPrefix.Length);
+            Array.Copy(buffer, 0, combined, lengthPrefix.Length, buffer.Length);
+
+            var tcs = new TaskCompletionSource<int>(_socket);
+
+            _socket.BeginSend(combined, 0, combined.Length, socketFlags, static ar =>
+            {
+                var _tcs = (TaskCompletionSource<int>)ar.AsyncState!;
+                var _socket = (Socket)_tcs.Task.AsyncState!;
+
+                try { _tcs.TrySetResult(_socket.EndSend(ar)); }
+                catch (Exception e) { _tcs.TrySetException(e); }
+            }, tcs);
+
+            return tcs.Task;
+        }
+
+        /// <summary>
+        /// Receives the next piece of data from the socket.
+        /// </summary>
+        /// <param name="socketFlags"></param>
+        /// <remarks><b>Warning:</b> Never use the base <see cref="System.Net.Sockets.Socket"/> Send/Receive methods
+        /// in conjunction with this class as they can corrupt the state.</remarks>
+        /// <returns>The next piece of full data sent by the external peer.</returns>
+        public Task<byte[]> ReceiveNextAsync(SocketFlags socketFlags)
+        {
+            // Receieve 4 bytes (int), which will tell us the length prefix of the next piece of data to download.
+            byte[] lengthPrefixInBytes = new byte[sizeof(int)];
+
+            var tcs = new TaskCompletionSource<byte[]>();
+            var state = new ReceiveState(tcs, lengthPrefixInBytes)
+            {
+                SocketFlags = socketFlags
+            };
+
+            _socket.BeginReceive(lengthPrefixInBytes, 0, lengthPrefixInBytes.Length, socketFlags, InternalReceiveLengthPrefix, state);
+
+            return tcs.Task;
+        }
+
+        private void InternalReceiveLengthPrefix(IAsyncResult ar)
+        {
+            ReceiveState state = (ReceiveState)ar.AsyncState!;
+
+            state.BytesRead += _socket.EndReceive(ar);
+
+            // Check if we have got the full 4 bytes for the length prefix.
+            if (state.BytesRead < sizeof(int))
+            {
+                _socket.BeginReceive(state.DataBuffer, state.BytesRead, state.DataBuffer.Length - state.BytesRead, state.SocketFlags, InternalReceiveLengthPrefix, state);
+                return;
+            }
+
+            int actualDataLength = BitConverter.ToInt32(state.DataBuffer);
+            state.BytesRead = 0;
+            state.DataBuffer = new byte[actualDataLength];
+
+            _socket.BeginReceive(state.DataBuffer, 0, state.DataBuffer.Length, state.SocketFlags, InternalReceiveNext, state);
+        }
+
+        private void InternalReceiveNext(IAsyncResult ar)
+        {
+            ReceiveState state = (ReceiveState)ar.AsyncState!;
+
+            state.BytesRead += _socket.EndReceive(ar);
+
+            if (state.BytesRead < state.DataBuffer.Length)
+            {
+                _socket.BeginReceive(state.DataBuffer, state.BytesRead, state.DataBuffer.Length - state.BytesRead, state.SocketFlags, InternalReceiveNext, state);
+                return;
+            }
+
+            try { state.TaskCompletionSource.TrySetResult(state.DataBuffer); }
+            catch (Exception e) { state.TaskCompletionSource.TrySetException(e); }
+        }
+    }
+}
---
+++ SocketExtensions/TaskSocketExtensions.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace Zintom.SocketExtensions
+{
+    /// <summary>
+    /// Provides a set of extension methods which return <see cref="Task"/> equivalents of common <see cref="System.Net.Sockets.Socket"/> functions.
+    /// </summary>
+    /// <remarks>Such as, Accept, Connect, Disconnect, Send, Receive</remarks>
+    public static class TaskSocketExtensions
+    {
+
+        /// <inheritdoc cref="Socket.Accept"/>
+        public static Task<Socket> AcceptAsyncTask(this Socket listener)
+        {
+            var tcs = new TaskCompletionSource<Socket>(listener);
+
+            listener.BeginAccept(static (ar) =>
+            {
+                var _tcs = (TaskCompletionSource<Socket>)ar.AsyncState!;
+                var _listener = (Socket)_tcs.Task.AsyncState!;
+
+                try { _tcs.TrySetResult(_listener.EndAccept(ar)); }
+                catch (Exception e) { _tcs.TrySetException(e); }
+            }, tcs);
+
+            return tcs.Task;
+        }
+
+        /// <inheritdoc cref="Socket.Disconnect(bool)"/>
+        public static Task DisconnectAsyncTask(this Socket socket, bool reuseSocket)
+        {
+            var tcs = new TaskCompletionSource(socket);
+
+            socket.BeginDisconnect(reuseSocket, static (ar) =>
+            {
+                var _tcs = (TaskCompletionSource)ar.AsyncState!;
+                var _socket = (Socket)_tcs.Task.AsyncState!;
+
+                try { _tcs.TrySetResult(); _socket.EndDisconnect(ar); }
+                catch (Exception e) { _tcs.TrySetException(e); }
+            }, tcs);
+
+            return tcs.Task;
+        }
+
+        /// <inheritdoc cref="Socket.Connect(IPAddress, int)"/>
+        public static Task ConnectAsyncTask(this Socket socket, IPAddress address, int port)
+        {
+            var tcs = new TaskCompletionSource(socket);
+
+            socket.BeginConnect(address, port, static (ar) =>
+            {
+                var _tcs = (TaskCompletionSource)ar.AsyncState!;
+                var _socket = (Socket)_tcs.Task.AsyncState!;
+
+                try { _tcs.TrySetResult(); _socket.EndConnect(ar); }
+                catch (Exception e) { _tcs.TrySetException(e); }
+            }, tcs);
+
+            return tcs.Task;
+        }
+
+        /// <inheritdoc cref="Socket.Send(byte[], int, int, SocketFlags)"/>
+        public static Task<int> SendAsyncTask(this Socket socket, byte[] buffer, int offset, int size, SocketFlags socketFlags)
+        {
+            // We have to pass the Socket and TCS as state objects because we are using a static
+            // anonymous function which cannot access locals or fields defined outside of it.
+            var tcs = new TaskCompletionSource<int>(socket);
+
+            // We use static anonymous functions as we do not want to capture any outside variables, capturing variables
+            // causes each captured variable to be allocated as a boxed object.
+            socket.BeginSend(buffer, offset, size, socketFlags, static (ar) =>
+            {
+                var t = (TaskCompletionSource<int>)ar.AsyncState!;
+                var s = (Socket)t.Task.AsyncState!;
+
+                try { t.TrySetResult(s.EndSend(ar)); }
+                catch (Exception e) { t.TrySetException(e); }
+            }, tcs);
+
+            return tcs.Task;
+        }
+
+        /// <inheritdoc cref="Socket.Receive(byte[], int, int, SocketFlags)"/>
+        public static Task<int> ReceiveAsyncTask(this Socket socket, byte[] buffer, int offset, int size, SocketFlags socketFlags)
+        {
+            var tcs = new TaskCompletionSource<int>(socket);
+
+            socket.BeginReceive(buffer, offset, size, socketFlags, static (ar) =>
+            {
+                var _tcs = (TaskCompletionSource<int>)ar.AsyncState!;
+                var _socket = (Socket)_tcs.Task.AsyncState!;
+
+                try { _tcs.TrySetResult(_socket.EndReceive(ar)); }
+                catch (Exception e) { _tcs.TrySetException(e); }
+            }, tcs);
+
+            return tcs.Task;
+        }
+
+    }
+}
